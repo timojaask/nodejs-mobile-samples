1853ad6e0ade747d1bd8a6270dd08ab6ac56cd2d
Timo Jaaskelainen <timojaask@gmail.com>
2020-04-04 21:57:28 -0400

    Add crashing sample

--- ios/native-xcode-node-folder/nodejs-project/main.js
+++ ios/native-xcode-node-folder/nodejs-project/main.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var leftPad = require('left-pad');
+var NodeTradfriClient = require("node-tradfri-client");
 var versions_server = http.createServer( (request, response) => {
   response.end('Versions: ' + JSON.stringify(process.versions) + ' left-pad: ' + leftPad(42, 5, '0'));
 });
